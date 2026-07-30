884fcdbbef80574a9e1aff0dc3196a7ecb32f8d5
fthr <ganzfathur9@gmail.com>
2026-07-30 11:36:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const AKUN_MANUAL = {
     "budi": { pass: "budi123", rank: "Bisnis", divisi: "Internal" },
     "rexy": { pass: "rexy11", rank: "Bisnis", divisi: "Internal" },
     "joko": { pass: "joko123", rank: "Associates", divisi: "Family" },
-    "memberbaru": { pass: "rahasia", rank: "Soldiers", divisi: "Family" }
+    "memberbaru": { pass: "rahasia", rank: "Soldiers", divisi: "Family" },
     "xoxo": { pass: "xoxo", rank: "Bisnis", divisi: "Family" }
 
 };
